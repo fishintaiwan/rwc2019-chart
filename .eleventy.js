b3b6655cae5f296c3975ef9faea73dade4fe1896
fishintaiwan <fershad@gmail.com>
2019-09-15 17:22:43 +0800

    update base and footer

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,9 @@ const pluginPWA = require("eleventy-plugin-pwa");
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("_includes/styles/*.css");
-    eleventyConfig.addPassthroughCopy("_includes/js/interact.js");
     eleventyConfig.addPassthroughCopy("_redirects");
     eleventyConfig.addPassthroughCopy("manifest.json");
+    eleventyConfig.addPassthroughCopy("_includes/icons");
 
     eleventyConfig.addFilter("addZone", require("./filters/zone.js") );
     eleventyConfig.addFilter("dayOffset", require("./filters/dayOffset.js"));
